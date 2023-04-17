84f63bb82147148701477a09ecc5c7546aa1d22a
anton <antofffka@inbox.ru>
2023-04-17 16:49:42 +0300

    added test, fixed method

--- src/main/java/javaIntensive/Main.java
+++ src/main/java/javaIntensive/Main.java
@@ -5,14 +5,5 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        MyArrayList<String> test = new MyArrayList<>();
-        test.add("Б");
-        test.add("А");
-        test.add("Е");
-        test.add("Г");
-        test.add("Д");
-        test.add("В");
-        test.quickSort(Comparator.naturalOrder());
-        test.display();
     }
 }
--- src/main/java/javaIntensive/MyArrayList.java
+++ src/main/java/javaIntensive/MyArrayList.java
@@ -2,11 +2,28 @@ package javaIntensive;
 
 import java.util.Comparator;
 
+/**
+ * Собственная реализация коллекции ArrayList
+ * @param <T> принимаемый тип элементов коллекции
+ */
 public class MyArrayList<T>{
+    /**
+     * Создание массива для хранения данных
+     */
     private T[] list;
+    /**
+     * Размер коллекции
+     */
     private int size;
+    /**
+     * Стартовый размер ArrayList
+     */
     private final int DEFAULT_CAPACITY = 10;
 
+    /**
+     * Конструкор с возможностью изменения размера коллекции
+     * @param capacity размер коллекции
+     */
     public MyArrayList(int capacity){
         if(capacity <= 0){
             throw new IllegalArgumentException("Capacity <= 0");
@@ -14,9 +31,18 @@ public class MyArrayList<T>{
             list = (T[]) new Object[capacity];
         }
     }
+
+    /**
+     * Конструктор без параметров
+     */
     public MyArrayList(){
         list = (T[]) new Object[DEFAULT_CAPACITY];
     }
+
+    /**
+     * Добавление элемента
+     * @param item элемент, который хотим добавить
+     */
     public void add(T item){
         //проверить переполнение
         if(size == list.length){
@@ -24,7 +50,16 @@ public class MyArrayList<T>{
         }
         list[size++] = item;
     }
+
+    /**
+     * Добавление элемента по индексу
+     * @param index номер, по которому добавляем
+     * @param item элемент, который добавляем
+     */
     public void add(int index, T item){
+        if(size == list.length){
+            sizeUp();
+        }
         for(int i = size; i > index; i--){
             list[i] = list[i -1];
         }
@@ -38,13 +73,23 @@ public class MyArrayList<T>{
             list = newItems;
         }
     }
+
+    /**
+     * Удаление элемента по индексу
+     * @param index номер элемента, который хотим удалить
+     */
     public void delete(int index){
 
         for (int i = index; i < size; i++){
             list[i] = list[i + 1];
         }
+        size--;
     }
 
+    /**
+     * Удаление конкретного элемента коллекции
+     * @param item элемент коллекции
+     */
     public void delete(T item){
         int pos = getIndex(item);
         if(pos == -1){
@@ -52,6 +97,12 @@ public class MyArrayList<T>{
         }
         delete(pos);
     }
+
+    /**
+     * Получение номера элемента коллекци по его значению
+     * @param item искомый элемент
+     * @return
+     */
     private int getIndex(T item){
         if(item ==  null){
             return -1;
@@ -63,11 +114,24 @@ public class MyArrayList<T>{
         }
         return -1;
     }
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * Печать списка элементов
+     */
     public void display(){
         for (T item: list) {
             System.out.println(item);
         }
     }
+
+    /**
+     * Получение элемента по индексу
+     * @param index номер элемента
+     * @return
+     */
     public T get(int index){
         //проверить индекс в границах
         if(index >= 0 && index < size) {
@@ -77,9 +141,21 @@ public class MyArrayList<T>{
             return null;
         }
     }
+
+    /**
+     * Изменение, редактирование элемента коллекции
+     * @param index номер элемента
+     * @param item элемент коллекции
+     */
     public void set(int index, T item){
         list[index] = item;
     }
+
+    /**
+     * Реализация быстрой сортировки, при помощи рекурсии
+     * Метод пребразует коллекцию в массив для дальнейшей сортировки
+     * @param comparator Способ сравнения элеметов
+     */
     public void quickSort(Comparator<T> comparator){
         T[] tempM = (T[]) new Object[size];
         for(int i = 0; i < size; i++){
@@ -89,13 +165,26 @@ public class MyArrayList<T>{
         clear();
         for(int i = 0; i < tempM.length; i++){
             list[i] = tempM[i];
+            size++;
         }
 
     }
+
+    /**
+     * Метод очищает колекцию
+     */
     private void clear(){
         list = (T[]) new Object[DEFAULT_CAPACITY];
         size = 0;
     }
+
+    /**
+     * Метод сортирует переданный массив
+     * @param arr массив
+     * @param begin начанльный элемент массива
+     * @param end последний элемент массива
+     * @param comparator способ сортировки
+     */
     private void massiveSort(T[] arr, int begin, int end, Comparator comparator){
         if(begin >= end){
             return;
